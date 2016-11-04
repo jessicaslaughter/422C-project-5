6915ebe1e9ba0e8451c7368c4a6e1a43aefeb17b
ginalu <ginalu@utexas.edu>
2016-11-04 18:04:22 -0500

    Make Critter and Time Step Button

--- Main.java
+++ Main.java
@@ -13,6 +13,8 @@ package assignment5; // cannot be in default package
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -20,6 +22,7 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene; 
 import javafx.scene.control.*; 
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -45,7 +48,7 @@ public class Main extends Application{
 			grid.setVgap(5);
 			grid.setGridLinesVisible(false);
 			grid.setPadding(new Insets(10, 10, 10, 10));
-			Scene scene = new Scene(grid, 500, 550);
+			Scene scene = new Scene(grid, 450, 350);
 			Text scenetitle = new Text("Critter World Controller");
 			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
 			grid.add(scenetitle, 0, 0, 20, 2);
@@ -59,50 +62,123 @@ public class Main extends Application{
 				        "Algae"
 				    );
 			ComboBox comboBox = new ComboBox(options);
-			grid.add(comboBox, 20, 4);
-			GridPane.setHalignment(comboBox, HPos.CENTER); // To align horizontally in the cell
+			grid.add(comboBox, 5, 4, 10, 1);
+			GridPane.setHalignment(comboBox, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(comboBox, VPos.BOTTOM); // To align vertically in the cell
+			
+			
 
 			Label num = new Label("Num of Critters:");
-			grid.add(num, 1, 5, 2, 1);
+			grid.add(num, 1, 5, 4, 1);
 			TextField numCritter = new TextField();
-			grid.add(numCritter, 20, 5);
+			grid.add(numCritter, 5, 5, 10 ,1);
 			Button butt = new Button("Add critters");
-			GridPane.setHalignment(butt, HPos.RIGHT); // To align horizontally in the cell
+			GridPane.setHalignment(butt, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(butt, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(butt, 20, 6);
-	        
-	        Label line = new Label(" ___________________________________________________________");
-	        grid.add(line, 1, 10, 20, 1);
+	        grid.add(butt, 15, 5);
+	        final Text actiontarget = new Text();
+	        grid.add(actiontarget, 5, 6, 20, 1);
+			GridPane.setHalignment(actiontarget, HPos.CENTER); // To align horizontally in the cell
+			GridPane.setValignment(actiontarget, VPos.BOTTOM); // To align vertically in the cell
+	        butt.setOnAction(new EventHandler<ActionEvent>() {
+	       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                if((comboBox.getValue() != null && 
+	                        !comboBox.getValue().toString().isEmpty()) && (numCritter.getText() != null && !numCritter.getText().isEmpty())){
+	                    String number = numCritter.getText();
+	                    String type = comboBox.getValue().toString();
+	                	if(!(numCritter.getText().matches("^[0-9]+$"))){
+		                	actiontarget.setFill(Color.FIREBRICK);
+		                    actiontarget.setText("Insert valid number");
+	                	}
+	                	else{
+	                		try{
+	                			actiontarget.setText("                   ");
+				                for(int i=0; i < Integer.parseInt(number); i+=1){
+	        							Critter.makeCritter(type);
+				                }
+	                		}catch(InvalidCritterException|NullPointerException|NumberFormatException exception){
+	            					System.out.println("error processing");
+	   
+	                		}
+	                	}
+	                }
+	                else{
+	                    actiontarget.setFill(Color.FIREBRICK);
+	                    actiontarget.setText("Please select number and type of critters");
+	                }
+	            }
+	        });
 	        
 			Label numStep = new Label("Num of Steps:");
-			grid.add(numStep, 1, 16, 2, 1);
+			grid.add(numStep, 1, 10, 4, 1);
 			TextField numSteps = new TextField();
-			grid.add(numSteps, 20, 16);
+			grid.add(numSteps, 5, 10, 10, 1);
 			Button buttStep = new Button("Time Steps");
-			GridPane.setHalignment(buttStep, HPos.RIGHT); // To align horizontally in the cell
+			GridPane.setHalignment(buttStep, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(buttStep, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(buttStep, 20, 17);
+	        grid.add(buttStep, 15, 10);
+	        final Text actiontarget1 = new Text();
+	        grid.add(actiontarget1, 5, 11, 20, 1);
+			GridPane.setHalignment(actiontarget1, HPos.CENTER); // To align horizontally in the cell
+			GridPane.setValignment(actiontarget1, VPos.BOTTOM); // To align vertically in the cell
+	        
+	        buttStep.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                if((numSteps.getText() != null && !numSteps.getText().isEmpty())){
+	                    String stepNum = numSteps.getText();
+	                	if(!(numSteps.getText().matches("^[0-9]+$"))){
+		                	actiontarget1.setFill(Color.FIREBRICK);
+		                    actiontarget1.setText("Insert valid number");
+	                	}
+	                	else{
+	                		try{
+	                			actiontarget1.setText("                   ");
+				                for(int i=0; i < Integer.parseInt(stepNum); i+=1){
+				                	Critter.worldTimeStep();
+				                }
+	                		}catch(NullPointerException|NumberFormatException exception){
+	            					System.out.println("error processing");
+	   
+	                		}
+	                	}
+	                }
+	                else{
+	                    actiontarget.setFill(Color.FIREBRICK);
+	                    actiontarget.setText("Please select number and type of critters");
+	                }
+	            }
+	        });
+	        
+	        
 	        
 			Button displayButt = new Button("Display World");
-			GridPane.setHalignment(displayButt, HPos.RIGHT); // To align horizontally in the cell
+			GridPane.setHalignment(displayButt, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(displayButt, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(displayButt, 1, 20);
+	        grid.add(displayButt, 5, 15);
 	        
 	        Label times = new Label("Time: ");
-	        grid.add(times, 19, 20, 2, 1);
+	        grid.add(times, 1, 15, 2, 1);
 	        GridPane.setHalignment(times, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(times, VPos.CENTER); // To align vertically in the cell
 	        
 			Label timesNum = new Label(String.valueOf(steps));
-	        grid.add(timesNum, 20, 20, 1, 1);
-	        GridPane.setHalignment(timesNum, HPos.CENTER); // To align horizontally in the cell
+	        grid.add(timesNum, 2, 15, 1, 1);
+	        GridPane.setHalignment(timesNum, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(timesNum, VPos.CENTER); // To align vertically in the cell
+			
+	        Label speedL = new Label("Speed: ");
+	        grid.add(speedL, 1, 23);
+	        GridPane.setHalignment(speedL, HPos.LEFT); // To align horizontally in the cell
+			GridPane.setValignment(speedL, VPos.CENTER); // To align vertically in the cell
 	        
 			Button runButt = new Button("Run");
 			GridPane.setHalignment(runButt, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(runButt, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(runButt, 1, 21);
+	        grid.add(runButt, 13, 23, 1, 1);
 	        
 	        Slider slider = new Slider();
 	        slider.setMin(0);
@@ -114,14 +190,22 @@ public class Main extends Application{
 	        slider.setMajorTickUnit(5);
 	        slider.setMinorTickCount(5);
 	        slider.setBlockIncrement(1);
-	        grid.add(slider, 2, 21);
+	        grid.add(slider, 2, 23, 10, 2);
 			GridPane.setHalignment(slider, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(slider, VPos.BOTTOM); // To align vertically in the cell
 
 			Button quitButt = new Button("Quit");
 			GridPane.setHalignment(quitButt, HPos.RIGHT); // To align horizontally in the cell
 			GridPane.setValignment(quitButt, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(quitButt, 20, 25);
+	        grid.add(quitButt, 15, 0, 1, 1);
+	       
+	        quitButt.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                System.exit(0);
+	            }
+	        });
 	        
 			primaryStage.show();
 			primaryStage.setScene(scene);
