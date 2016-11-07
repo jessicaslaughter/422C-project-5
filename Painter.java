b80afc02083178da4702bb55b2400e4b9c350ddc
Jessica <jessicaslaughter@utexas.edu>
2016-11-06 18:11:15 -0600

    added triangle and diamond, fixed scaling

--- Painter.java
+++ Painter.java
@@ -14,33 +14,90 @@ public class Painter {
 				break; 
 			case CIRCLE: s = new Circle(size/2); 
 				break;
+			case TRIANGLE: s = drawTriangle();
+				break;
+			case DIAMOND: s = drawDiamond();
+				break;
+			case STAR: s = drawStar();
 		default:
 			break;
 		} 
 		return s;
 	}
 	
-	public static int setSize() {
+	public static void setSize() {
 		// scale grid
-		if (Params.world_width <= 30 || Params.world_height <= 30) {
-			size = 20;
+		if (Params.world_width <= 50) {
+			if (Params.world_height <= 30) {
+				size = 18;
+			}
+			else if (Params.world_height <= 50) {
+				size = 12;
+			}
+			else if (Params.world_height <= 75) {
+				size = 8;
+			}
+			else if (Params.world_height <= 100) {
+				size = 6;
+			}
+			else if (Params.world_height <= 130) {
+				size = 4;
+			}
+			else {
+				size = 3;
+			}
 		}
-		else if (Params.world_width <= 50 || Params.world_height <= 50) {
-			size = 10;
+		else if (Params.world_width <= 100) {
+			if (Params.world_height <= 50) {
+				size = 10;
+			}
+			else if (Params.world_height <= 75) {
+				size = 8;
+			}
+			else if (Params.world_height <= 100) {
+				size = 6;
+			}
+			else if (Params.world_height <= 130) {
+				size = 4;
+			}
+			else {
+				size = 3;
+			}
 		}
-		else if (Params.world_width <= 75 || Params.world_height <= 75) {
-			size = 8;
+		else if (Params.world_width <= 150) {
+			if (Params.world_height <= 50) {
+				size = 7;
+			}
+			else if (Params.world_height <= 75) {
+				size = 6;
+			}
+			else if (Params.world_height <= 100) {
+				size = 5;
+			}
+			else if (Params.world_height <= 130) {
+				size = 4;
+			}
+			else {
+				size = 3;
+			}
 		}
-		else if (Params.world_width <= 100 || Params.world_height <= 100) {
-			size = 6;
-		}
-		else if (Params.world_width <= 150 || Params.world_height <= 150) {
-			size = 4;
+		else if (Params.world_width <= 200) {
+			if (Params.world_height <= 50) {
+				size = 6;
+			}
+			else if (Params.world_height <= 100) {
+				size = 5;
+			}
+			else if (Params.world_height <= 130) {
+				size = 4;
+			}
+			else {
+				size = 3;
+			}
 		}
 		else {
 			size = 3;
 		}
-		return size;
 	}
 	/* Paints the shape on a grid. */ 
 	public static void paint() { 
@@ -61,14 +118,31 @@ public class Painter {
 		Shape s = getIcon(shape);
 		if (color.equals(fill)) {
 			s.setFill(fill);
+			if (outline.equals(Color.WHITE)) {
+				s.setStroke(fill);
+			}
+			else {
+				s.setStroke(outline);
+			}
 		}
 		else if (color.equals(Color.WHITE)) {
 			s.setFill(fill);
+			if (outline.equals(Color.WHITE)) {
+				s.setStroke(fill);
+			}
+			else {
+				s.setStroke(outline);
+			}
 		}
 		else {
 			s.setFill(color);
+			if (outline.equals(Color.WHITE)) {
+				s.setStroke(color);
+			}
+			else {
+				s.setStroke(outline);
+			}
 		}
-		s.setStroke(outline);
 		Main.modelGrid.add(s, x, y);
 	}
 	
@@ -76,10 +150,49 @@ public class Painter {
 		for (int i = 0; i < Params.world_width; i++) {
 			for (int j = 0; j < Params.world_height; j++) {
 				Shape s = new Rectangle(size, size); 
-				s.setFill(null);
+				s.setFill(Color.WHITE);
 				s.setStroke(Color.BLACK); 
 				Main.modelGrid.add(s, i, j);
 			}
 		}
+	}
+	
+	private static Shape drawTriangle() {
+		Polygon triangle = new Polygon();
+		triangle.getPoints().addAll(new Double[]{
+			    (double)size/2.0, 1.0,
+			    (double)size - 1.0, (double)size - 1.0,
+			    1.0, (double)size-1.0
+		});
+		return triangle;
+	}
+	
+	private static Shape drawDiamond() {
+		Polygon diamond = new Polygon();
+		diamond.getPoints().addAll(new Double[]{
+				(double)size/2.0, 1.0,
+				(double)size - 1.0, (double)size/2.0,
+				(double)size/2.0, (double)size - 1.0,
+				1.0, (double)size/2.0
+		});
+		return diamond;
+	}
+	
+	// need to fix
+	private static Shape drawStar() {
+		Polygon star = new Polygon();
+		star.getPoints().addAll(new Double[]{
+				(double)size/2.0, 1.0,
+				(double)size/2.0 + (double)size/4.0, (double)size/3.0,
+				(double)size - 1.0, (double)size/2.0,
+				(double)size/2.0 + (double)size/4.0, (double)size/2.0 + 1.0,
+			    (double)size - 1.0, (double)size - 1.0,
+			    (double)size/2.0, (double)size/2.0 + (double)size/4.0,
+			    1.0, (double)size-1.0,
+			    (double)size/2.0 - (double)size/4.0, (double)size/2.0 + 1.0,
+			    2.0, (double)size/2.0
+				
+		});
+		return star;
 	}
 }
