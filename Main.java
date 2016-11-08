9cbfb6ee6160bede187d6ca85efbf7a0a7915dac
ginalu <ginalu@utexas.edu>
2016-11-08 01:40:00 -0600

    still need to add in stats

--- Main.java
+++ Main.java
@@ -10,6 +10,10 @@
  * Fall 2016
  */
 package assignment5; // cannot be in default package
+import java.io.File;
+
+import javax.crypto.spec.RC2ParameterSpec;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -49,7 +53,7 @@ public class Main extends Application{
 			grid.setVgap(5);
 			grid.setGridLinesVisible(false);
 			grid.setPadding(new Insets(10, 10, 10, 10));
-			Scene scene = new Scene(grid, 400, 400);
+			Scene scene = new Scene(grid, 450, 400);
 			Text scenetitle = new Text("Critter World Controller");
 			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
 			grid.add(scenetitle, 0, 0, 20, 2);
@@ -63,18 +67,30 @@ public class Main extends Application{
 			Label name = new Label("Critter Name:");
 			grid.add(name, 1, 4, 2, 1);
 			
+
 			ObservableList<String> options = 
-				    FXCollections.observableArrayList(
-				        "Craig",
-				        "Algae"
-				    );
+				    FXCollections.observableArrayList();
 			ComboBox comboBox = new ComboBox(options);
 			grid.add(comboBox, 5, 4, 10, 1);
 			GridPane.setHalignment(comboBox, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(comboBox, VPos.BOTTOM); // To align vertically in the cell
 			
+			File[] files = new File("./src/assignment5").listFiles();
+			for(File file: files){
+				String fileName = file.getName();
+				if(file.isFile() && fileName.contains(".java")){
+					try{
+						String[] fName = fileName.split(".java");
+						String packageName = Critter.class.getPackage().toString().split(" ")[1];
+						Critter c = (Critter) Class.forName(packageName + "." + fName[0]).newInstance();
+						options.add(fName[0]);
+					}catch(Exception exception){
+					}
+					
+				}
+			}
 			
-
+			
 			Label num = new Label("Num of Critters:");
 			grid.add(num, 1, 5, 4, 1);
 			TextField numCritter = new TextField();
@@ -105,7 +121,8 @@ public class Main extends Application{
 				                for(int i=0; i < Integer.parseInt(number); i+=1){
 	        							Critter.makeCritter(type);
 				                }
-	                		}catch(InvalidCritterException|NullPointerException|NumberFormatException exception){
+				                Critter.displayWorld();
+	                		}catch(Exception exception){
 	            					System.out.println("error processing");
 	   
 	                		}
@@ -115,38 +132,39 @@ public class Main extends Application{
 	                    actiontarget.setFill(Color.FIREBRICK);
 	                    actiontarget.setText("Please select number and type of critters");
 	                }
+	            	comboBox.getSelectionModel().clearSelection();
+	            	numCritter.clear();
 	            }
 	        });
 	        
-			Label numStep = new Label("Num of Steps:");
-			grid.add(numStep, 1, 10, 4, 1);
-			TextField numSteps = new TextField();
-			grid.add(numSteps, 5, 10, 10, 1);
-			Button buttStep = new Button("Time Steps");
-			GridPane.setHalignment(buttStep, HPos.LEFT); // To align horizontally in the cell
-			GridPane.setValignment(buttStep, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(buttStep, 15, 10);
-	        final Text actiontarget1 = new Text();
-	        grid.add(actiontarget1, 5, 11, 20, 1);
-			GridPane.setHalignment(actiontarget1, HPos.CENTER); // To align horizontally in the cell
-			GridPane.setValignment(actiontarget1, VPos.BOTTOM); // To align vertically in the cell
-	        
-	        buttStep.setOnAction(new EventHandler<ActionEvent>() {
+			Label seedsNum = new Label("Seeds:");
+			grid.add(seedsNum, 1, 9, 4, 1);
+			TextField numSeeds = new TextField();
+			grid.add(numSeeds, 5, 9, 10 ,1);
+			Button buttSeed = new Button("Add Seeds");
+			GridPane.setHalignment(buttSeed, HPos.LEFT); // To align horizontally in the cell
+			GridPane.setValignment(buttSeed, VPos.BOTTOM); // To align vertically in the cell
+	        grid.add(buttSeed, 15, 9);
+	        final Text actiontarget3 = new Text();
+	        grid.add(actiontarget3, 5, 10, 20, 1);
+			GridPane.setHalignment(actiontarget3, HPos.CENTER); // To align horizontally in the cell
+			GridPane.setValignment(actiontarget3, VPos.BOTTOM); // To align vertically in the cell
+			
+	        buttSeed.setOnAction(new EventHandler<ActionEvent>() {
 		       	 
 	            @Override
 	            public void handle(ActionEvent event) {
-	                if((numSteps.getText() != null && !numSteps.getText().isEmpty())){
-	                    String stepNum = numSteps.getText();
-	                	if(!(numSteps.getText().matches("^[0-9]+$"))){
-		                	actiontarget1.setFill(Color.FIREBRICK);
-		                    actiontarget1.setText("Insert valid number");
+	                if((numSeeds.getText() != null && !numSeeds.getText().isEmpty())){
+	                    String seedNum = numSeeds.getText();
+	                	if(!(numSeeds.getText().matches("^[0-9]+$"))){
+		                	actiontarget3.setFill(Color.FIREBRICK);
+		                    actiontarget3.setText("Insert valid number");
 	                	}
 	                	else{
 	                		try{
-	                			actiontarget1.setText("                   ");
-				                for(int i=0; i < Integer.parseInt(stepNum); i+=1){
-				                	Critter.worldTimeStep();
-				                }
+	                			actiontarget3.setText("                   ");
+	                			long seedNum0 = Long.parseLong(seedNum);
+				                Critter.setSeed(seedNum0);
 	                		}catch(NullPointerException|NumberFormatException exception){
 	            					System.out.println("error processing");
 	   
@@ -154,27 +172,39 @@ public class Main extends Application{
 	                	}
 	                }
 	                else{
-	                    actiontarget.setFill(Color.FIREBRICK);
-	                    actiontarget.setText("Please select number and type of critters");
+	                    actiontarget3.setFill(Color.FIREBRICK);
+	                    actiontarget3.setText("Please select number of seeds");
 	                }
 	            }
 	        });
 	        
+			Label numStep = new Label("Num of Steps:");
+			grid.add(numStep, 1, 12, 4, 1);
+			TextField numSteps = new TextField();
+			grid.add(numSteps, 5, 12, 10, 1);
+			Button buttStep = new Button("Time Steps");
+			GridPane.setHalignment(buttStep, HPos.LEFT); // To align horizontally in the cell
+			GridPane.setValignment(buttStep, VPos.BOTTOM); // To align vertically in the cell
+	        grid.add(buttStep, 15, 12);
+	        final Text actiontarget1 = new Text();
+	        grid.add(actiontarget1, 5, 13, 20, 1);
+			GridPane.setHalignment(actiontarget1, HPos.CENTER); // To align horizontally in the cell
+			GridPane.setValignment(actiontarget1, VPos.BOTTOM); // To align vertically in the cell
+	       
+	        
 	        
 	        
 			Button displayButt = new Button("Display World");
-			GridPane.setHalignment(displayButt, HPos.LEFT); // To align horizontally in the cell
+			GridPane.setHalignment(displayButt, HPos.RIGHT); // To align horizontally in the cell
 			GridPane.setValignment(displayButt, VPos.BOTTOM); // To align vertically in the cell
-	        grid.add(displayButt, 5, 15);
+	        grid.add(displayButt, 14, 0, 1, 1);
 	        
 	        displayButt.setOnAction(new EventHandler<ActionEvent>() {
 		       	 
 	            @Override
 	            public void handle(ActionEvent event) {
-	            	numSteps.clear();
-	            	numCritter.clear();
-	            	comboBox.getSelectionModel().clearSelection();
-	                Critter.displayWorld();
+                	Critter.clearWorld();
+                	Critter.displayWorld();
 	            }
 	        });
 	        
@@ -184,9 +214,10 @@ public class Main extends Application{
 	        GridPane.setHalignment(times, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(times, VPos.CENTER); // To align vertically in the cell
 	        
-			Label timesNum = new Label(String.valueOf(steps));
+	        final Text timesNum = new Text();
+	        timesNum.setText(String.valueOf(steps));
 	        grid.add(timesNum, 2, 15, 1, 1);
-	        GridPane.setHalignment(timesNum, HPos.LEFT); // To align horizontally in the cell
+			GridPane.setHalignment(timesNum, HPos.LEFT); // To align horizontally in the cell
 			GridPane.setValignment(timesNum, VPos.CENTER); // To align vertically in the cell
 			
 	        Label speedL = new Label("Speed: ");
@@ -217,6 +248,46 @@ public class Main extends Application{
 			GridPane.setHalignment(quitButt, HPos.RIGHT); // To align horizontally in the cell
 			GridPane.setValignment(quitButt, VPos.BOTTOM); // To align vertically in the cell
 	        grid.add(quitButt, 15, 0, 1, 1);
+	        
+			Button statsButton = new Button("Stats");
+			GridPane.setHalignment(statsButton, HPos.LEFT); // To align horizontally in the cell
+			GridPane.setValignment(statsButton, VPos.BOTTOM); // To align vertically in the cell
+	        grid.add(statsButton, 15, 23, 1, 1);
+	        
+	        buttStep.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                if((numSteps.getText() != null && !numSteps.getText().isEmpty())){
+	                    String stepNum = numSteps.getText();
+	                	if(!(numSteps.getText().matches("^[0-9]+$"))){
+		                	actiontarget1.setFill(Color.FIREBRICK);
+		                    actiontarget1.setText("Insert valid number");
+	                	}
+	                	else{
+	                		try{
+	                			actiontarget1.setText("                   ");
+				                for(int i=0; i < Integer.parseInt(stepNum); i+=1){
+				                	Critter.worldTimeStep();
+				                	//Critter.clearWorld();
+				                	Critter.displayWorld();
+				                }
+				                steps += Integer.parseInt(stepNum);
+				                timesNum.setText("          ");
+				                timesNum.setText(String.valueOf(steps));
+	                		}catch(NullPointerException|NumberFormatException exception){
+	            					System.out.println("error processing");
+	   
+	                		}
+	                	}
+	                }
+	                else{
+	                    actiontarget1.setFill(Color.FIREBRICK);
+	                    actiontarget1.setText("Please select number of steps");
+	                }
+	            	numSteps.clear();
+	            }
+	        });
 	       
 	        quitButt.setOnAction(new EventHandler<ActionEvent>() {
 		       	 
